8bc8d12f025bb856cad5d3514df69f5c6e1dcf8d
ashwin2200 <ashwn73@gmail.com>
2021-04-07 18:08:52 -0700

    feat: add listing for specific item with selctor

--- restful/api.py
+++ restful/api.py
@@ -8,14 +8,17 @@ def index():
     return render_template('index.html')
 
 # Import here to avoid circular import
-from restful.entry import list_names
+from restful.entry import list_names, fetch_item_for
 
 # List all individuals & cars
 @api.route('/v1/<EntryType>/all', methods=['GET'])
 def list_entry_names(EntryType):
     reply = list_names(EntryType)
-
     return reply
 
 # List all data for specified car/individual
-# @api.route('/v1/<EntryType>/<Selector>/all', methods=['GET'])
+@api.route('/v1/<EntryType>/<Selector>/all', methods=['GET'])
+def get_data(EntryType, Selector):
+    reply = fetch_item_for(EntryType, Selector)
+
+    return reply
--- restful/entry.py
+++ restful/entry.py
@@ -3,6 +3,45 @@ from flask import jsonify
 from restful.configuration import ADMIN_TOKEN
 from restful.data_models import FuelEntry, GymEntry
 
+def fetch_item_for(EntryType, Selector):
+
+        reply = []
+
+        if EntryType == "Fuel":
+            entries = FuelEntry.query.filter_by(carName=Selector).all()
+
+            for entry in entries:
+                item = {
+                    "carName":Selector,
+                    "date": entry.date,
+                    "mileage": entry.mileage,
+                    "distanceTravelledBetweenEntry": entry.distanceTravelledBetweenEntry,
+                    "gasTotal": entry.gasTotal,
+                    "priceTotal": entry.priceTotal,
+                    "MPG": entry.MPG
+                }
+                reply.append(item)
+        
+        elif EntryType == "Gym":
+            entries = GymEntry.query.filter_by(individualName=Selector).all()
+
+            for entry in entries:
+                item = {
+                    "individualName":Selector,
+                    "date": entry.date,
+                    "bodyPhotoABS": entry.bodyPhotoABS,
+                    "individualWeight": entry.individualWeight,
+                    "startTime": entry.startTime,
+                    "endTime": entry.endTime,
+                    "caloriesExpended": entry.caloriesExpended
+                }
+                reply.append(item)
+        
+        if len(reply) == 0:
+            return f"No data for {Selector} in table {EntryType}"
+        
+        return reply
+
 def list_names(EntryType):
     
     reply = []
