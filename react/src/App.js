ae36f18da85ddcc60529c08a783eee8ea53bc604
LINGFENG LIANG <40837859+davidliang2019@users.noreply.github.com>
2021-04-10 23:54:31 -0700

    add router

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,24 @@
 import Header from './components/Header'
+import Overview from './components/Overview'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="Container">
-      <Header/>
+    <Router>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/overview" component={Overview} />
+      </Switch>
     </div>
+  </Router>
   );
 }
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
