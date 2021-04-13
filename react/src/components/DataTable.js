4c14987e27204bac63910f83b2cfce0d52b82b1b
LINGFENG LIANG <40837859+davidliang2019@users.noreply.github.com>
2021-04-13 15:47:43 -0700

    set up the get request

--- react/src/components/DataTable.js
+++ react/src/components/DataTable.js
@@ -1,8 +1,31 @@
+import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const DataTable= () => {
+    const path = 'https://api.ashwin.lol/damocles/v1' + useLocation().pathname
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+    const [info, setInfo] = useState([])
+    useEffect(() => {
+        axios.get(path)
+            .then(response => {
+                setLoading(false)
+                setInfo(response.data)
+                setError('')
+            })
+            .catch(error => {
+                setLoading(false)
+                setInfo([])
+                setError('Something went wrong!')
+            })
+    },[path])
+
     return (
         <header>
             <h1>Data Table</h1>
-            <hr/>
+            {loading ? 'Loading': console.log(info)}
+            {error ? error : null}
         </header>
     )
 }
--- react/src/components/Overview.js
+++ react/src/components/Overview.js
@@ -3,15 +3,15 @@ import { Card, Container, Row, Col } from 'react-bootstrap'
 import { menuTree } from '../App'
 const Overview = () => {
     const pathList = useLocation().pathname.split('/').filter(Boolean)
-    const submenu = menuTree.filter(menu => menu.name === pathList[0])[0].list;
+    const submenu = menuTree.filter(menu => menu.name === pathList[0])[0];
     return (
         <header>
             <h1>Overview</h1>
             <Container>
                 <Row>
-                {submenu.map((item,index) => (
+                {submenu.list.map((item,index) => (
                     <Col key={index}>
-                    <Link to={"/" + pathList[0] + "/" + item} style={{ textDecoration: 'none' }}>
+                    <Link to={"/" + submenu.name + "/" + item} style={{ textDecoration: 'none' }}>
                         <Card border = 'primary'>
                         <Card.Body>
                             <Card.Text>{item}</Card.Text>
