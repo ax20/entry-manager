9b7f9d235092a638b301c2c51441a44cc8aa59bb
ashwin2200 <ashwn73@gmail.com>
2021-04-13 18:20:03 -0700

    build: base

--- api.py
+++ api.py
@@ -1,36 +1,17 @@
+"""
+Name: Main App
+Description: Brings all the classes together and hosts the server and handles all requests
+Dependencies:
+- Flask (The app that runs the server)
+- Config (To fetch the url location of the API)
+"""
 from flask import Flask, request, render_template
+from config import api, app
 
-api = Flask(__name__)
-api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# flask instance
+API = Flask(__name__)
+API.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@api.route('/')
+@API.route(api['location'])
 def index():
-    return render_template('index.html')
-
-# Import here to avoid circular import
-from restful.entry import list_names, fetch_item_for, new_item
-
-# List all individuals & cars
-@api.route('/v1/<EntryType>/all', methods=['GET'])
-def list_entry_names(EntryType):
-    reply = list_names(EntryType)
-    return reply
-
-# List all data for specified car/individual
-@api.route('/v1/<EntryType>/<Selector>/', methods=['GET'])
-def get_data(EntryType, Selector):
-    reply = fetch_item_for(EntryType, Selector)
-    return reply
-
-@api.route('/v1/<EntryType>/<Selector>/', methods=['POST'])
-def new_data(EntryType, Selector):
-    reply = new_item(EntryType, Selector, request.get_json())
-    return reply
-
-@api.route(f'/{API_LABEL}/v1/<EntryType>/', methods=['DELETE'])
-def delete_data(EntryType):
-    reply = delete_entry(EntryType, request.get_json())
-    return reply
-
-if __name__ == '__main__':
-    api.run()
+    return render_template('index.htm', title=app['name'] + " v" + app['version'])
