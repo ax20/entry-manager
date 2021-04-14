02982da4776106e03a8955ed594a61888090a258
LINGFENG LIANG <40837859+davidliang2019@users.noreply.github.com>
2021-04-13 20:27:33 -0700

    Update DataTable.js

--- react/src/components/DataTable.js
+++ react/src/components/DataTable.js
@@ -1,9 +1,11 @@
-import { useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+//import { useLocation } from 'react-router-dom'
+import { Fragment, useEffect, useState } from 'react'
+import { Table } from 'react-bootstrap'
 import axios from 'axios'
 
 const DataTable= () => {
-    const path = 'https://api.ashwin.lol/damocles/v1' + useLocation().pathname
+    //const path = 'https://cors-anywhere.herokuapp.com/https://api.ashwin.lol/damocles/v1' + useLocation().pathname
+    const path = 'https://jsonplaceholder.typicode.com/posts/'
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState('')
     const [info, setInfo] = useState([])
@@ -22,11 +24,31 @@ const DataTable= () => {
     },[path])
 
     return (
-        <header>
-            <h1>Data Table</h1>
-            {loading ? 'Loading': console.log(info)}
-            {error ? error : null}
-        </header>
+        <Fragment>
+            {console.log(info)}
+        <h1>Data Table</h1>
+        <Table striped bordered hover>
+            <thead>
+            <tr>
+                <th>UserId</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Body</th>
+            </tr>
+            </thead>
+            <tbody>
+                {loading ? null: info.map(item=>(
+                        <tr key={item.id}>
+                            <td>{item.userId}</td>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.body}</td>
+                        </tr>
+                ))}
+                {error ? error : null}
+            </tbody>
+        </Table>
+        </Fragment>
     )
 }
 
