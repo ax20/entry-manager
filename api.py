9f0c379143151d6f74609da5e66a5e69d82fab5a
ashwin2200 <ashwn73@gmail.com>
2021-04-14 19:42:26 -0700

    build: add entry management & location

--- api.py
+++ api.py
@@ -6,12 +6,55 @@ Dependencies:
 - Config (To fetch the url location of the API)
 """
 from flask import Flask, request, render_template
-from config import api, app
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import os.path, hashlib, random, string
+from config import api, app, database
 
-# flask instance
 API = Flask(__name__)
 API.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# CORS bypass
+bypassees = app['cors-bypass']
+CORS(API, resources={r"/*": bypassees})
 
+# routes
 @API.route(api['location'])
 def index():
     return render_template('index.htm', title=app['name'] + " v" + app['version'])
+
+from handler import data_new, data_request, list_names
+
+@API.route(api['location'] + "<Model>/<Name>", methods=['GET', 'POST'])
+def handle_data(Model, Name):
+    
+    if request.method == "POST":
+        return data_new(Model, Name, request.get_json())
+    elif request.method == "GET":
+        return data_request(Model, Name)
+    else:
+        return "405"
+
+@API.route(api['location'] + "<Model>/list", methods=['GET'])
+def list_model_names(Model):
+    list_names(Model)
+
+@API.route(api['location'] + "upload", methods=['POST'])
+def upload_image():
+    
+    UPLOAD_FOLDER = 'static/media/'
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+    if 'file' not in request.files:
+        print('No file part')
+    file = request.files['file']
+    if file.filename == '':
+        print('No selected file')
+    if file:
+        if os.path.splitext(file.filename)[1][1:] in ALLOWED_EXTENSIONS:
+           fn = file.filename + "_".join(random.choices(string.ascii_uppercase + string.digits, k=10))
+           fn = hashlib.md5(fn.encode()).hexdigest()
+           filename = secure_filename(fn + "." + os.path.splitext(file.filename)[1][1:])
+           file.save(os.path.join(UPLOAD_FOLDER, filename))
+           path = os.path.dirname(os.path.abspath(__file__)).replace(chr(92), chr(47)) + chr( 47) + os.path.join(UPLOAD_FOLDER, filename)
+           return path
+            
+    return """<h1>Upload new File</h1><form enctype=multipart/form-data method=post><input type="file" name="file"> <input type=submit></form>"""
---
+++ handler.py
@@ -0,0 +1,159 @@
+from flask import jsonify
+from config import api
+from  datetime import  datetime
+from database import FuelEntry, GymEntry, DB, create_txn, delete_txn
+
+def calculate_time_diffrence(hour_a, hour_b, minute_a, minute_b):
+    if (hour_a == hour_b):
+        return minute_b-minute_a
+    else:
+        return ((hour_b - hour_a) * 60 + (60 * minute_a) + minute_b)
+
+def calculate_distance_travelled(mileage, carName):
+    entries = FuelEntry.query.filter_by(carName=carName).order_by(sqlalchemy.desc(FuelEntry.date)).all()
+    if len(entries) > 0:
+        mileage_latest = entries[-1].mileage
+        return (int(mileage) - int(mileage_latest))
+    else:
+        return 0
+
+def calculate_mpg(distance, gas):
+    return round((distance/gas), 2)
+
+def set_date(date):
+    if date == "null":
+        return datetime.utcnow()
+    else:
+        delta = datetime.utcnow()-datetime.now()
+        j = date.split('/')
+        return datetime(int(j[2]), int(j[1]), int(j[0]), 0, 0, 0) + delta
+
+def calculate_calories(time):
+    rate = 520 # with a bmi of 23  you expend ~520 calories at the gym of medium/intense workout
+    return (time/60) * time
+
+def data_request(Model, Name):
+    reply = []
+
+    if Model == "Fuel":
+        entries = FuelEntry.query.filter_by(carName=Name).all()
+
+        for entry in entries:
+
+            item = {
+                    "id": entry.entry_id,
+                    "carName":Name,
+                    "date": entry.date,
+                    "mileage": entry.mileage,
+                    "distanceTravelledBetweenEntry": entry.distanceTravelledBetweenEntry,
+                    "gasTotal": entry.gasTotal,
+                    "priceTotal": entry.priceTotal,
+                    "MPG": entry.MPG
+                }
+            
+            reply.append(item)
+    
+    elif Model == "Gym":
+        entries = GymEntry.query.filter_by(individualName=Name).all()
+        
+        for entry in entries:
+            
+            item = {
+                "id": entry.entry_id,
+                "individualName":Name,
+                "date": entry.date,
+                "bodyPhotoABS": entry.bodyPhotoABS,
+                "individualWeight": entry.individualWeight,
+                "startTime": entry.startTime,
+                "endTime": entry.endTime,
+                "caloriesExpended": entry.caloriesExpended
+            }
+            
+            reply.append(item)
+    else:
+        return "422"
+    
+    if len(reply) == 0:
+        return "204"
+    
+    return jsonify(reply)
+
+def data_new(Model, Name, json):
+
+    # authorization
+    if json.get('secret') == None or json.get('secret') != api['token']:
+        return "Unauthorized"
+
+    # fuel entry
+    if Model == "Fuel":
+        item = FuelEntry(
+            carName = Name,
+            date = set_date(json['date']),
+            mileage = json['mileage'],
+            distanceTravelledBetweenEntry = calculate_distance_travelled(mileage, Name),
+            gasTotal = json['gasTotal'],
+            priceTotal = json['priceTotal'],
+            MPG = calculate_mpg(calculate_distance_travelled(mileage, Name), gasTotal)
+        )
+    
+    elif Model == "Gym":
+
+        time_spent = calculate_time_diffrence(startTime[0:1], endTime[0:1], startTime[2:3], endTime[2:3])
+
+        item = GymEntry(
+            individualName=Name,
+            date = set_date(json['date']),
+            bodyPhotoABS = json['bodyPhotoABS'],
+            individualWeight = json['individualWeight'],
+            startTime = json['startTime '],
+            endTime = json['endTime'],
+            calculate_calories = calculate_calories(time_spent)
+        )
+    else:
+        return "422"
+    
+    create_txn(item)
+    return "Created " + Model + " => " + item.entry_id
+
+def list_names(Model):
+    reply = []
+
+    if Model == "Fuel":
+        data = FuelEntry.query.all()
+        names = {}
+        
+        if len(data) > 1:
+            for entry in data:
+                name = entry.carName
+                if name in names:
+                    names.append(name)
+                else:
+                    continue
+
+    elif Model == "Gym":
+        data = GymEntry.query.all()
+        names = {}
+        
+        if len(data) > 1:
+            for entry in data:
+                name = entry.individualName
+                if name not in names:
+                    names.append(name)
+                else:
+                    continue
+
+    else:
+        return "422"
+                
+    reply.append(names)
+
+    if len(reply) == 0:
+        return "204"
+
+    return jsonify(reply)
+
+def data_update(Model, Name, json):
+    return ""
+
+def data_delete(Model, Name, json):
+    return ""
