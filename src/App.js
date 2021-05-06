bc8247a0cd321f61c3bf52635568b19190b15fcc
LINGFENG LIANG <40837859+davidliang2019@users.noreply.github.com>
2021-05-06 16:45:05 -0700

    reduce size

--- src/App.js
+++ src/App.js
@@ -1,69 +1,16 @@
-import Header from './components/Header'
-import Overview from './components/Overview'
-import DataTable from './components/DataTable'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import { Card, Container, Row, Col } from 'react-bootstrap'
-import { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-
-export const menuTree = [{
-  'name':'Fuel',
-  'list':['Matryx', 'two']
-},{
-  'name':'Gym',
-  'list':['one', 'two']
-}]
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './components/Home'
 function App() {
   return (
-    <BrowserRouter>
-      <Container>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          {menuTree.map((items,index) => (
-            <Fragment key={index}>
-              <Route path= {"/" + items.name} exact component={Overview} />
-              {items.list.map((subItems, index)=>(
-                <Fragment key={index}>
-                  <Route path= {"/" +items.name+"/"+ subItems} exact component={DataTable} />
-                </Fragment>
-              ))}
-            </Fragment>
-          ))}
-          <Route component={My404Component} />
-        </Switch>
-      </Container>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
-
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-    <Container>
-      <Row>
-      {menuTree.map((items,index) => (
-        <Col key={index}>
-          <Link to={"/" + items.name} style={{ textDecoration: 'none' }}>
-            <Card border = 'primary'>
-            <Card.Body>
-                <Card.Text>{items.name}</Card.Text>
-            </Card.Body>
-            </Card>
-          </Link>
-        </Col>
-      ))}
-      </Row>
-    </Container>
-  </div>
-)
-
-const My404Component = () => (
-  <Container>
-    404
-  </Container>
-)
 
 export default App;
