596b986c895c410fec5323518ffc0c8c6cec437b
LINGFENG LIANG <40837859+davidliang2019@users.noreply.github.com>
2021-05-07 16:28:00 -0700

    small update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './components/Home'
 import useController from './hooks/useController'
-import useFormInput from './hooks/useFormInput'
 
 export const ControlContext = React.createContext()
 
 function App() {
-  const url = 'http://localhost:5000'
-  const carname = useFormInput('Nissan')
-  const mileage = useFormInput(10000)
-  const total = useFormInput(1000.0)
-  const gastotal = useFormInput(1000.0)
-  const controller = useController(url,carname,mileage,total,gastotal)
+  const controller = useController('http://localhost:5000')
   return (
     <ControlContext.Provider value = {controller}>
       <BrowserRouter>
--- src/hooks/useController.js
+++ src/hooks/useController.js
@@ -1,63 +1,78 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+  return {
+    value,
+    handleChange,
+    setValue
+  }
+}
+function useController(url){
+  const carname = useFormInput('Nissan')
+  const mileage = useFormInput(10000)
+  const total = useFormInput(1000.0)
+  const gastotal = useFormInput(1000.0)
 
-function useController(url,carname,mileage,total,gastotal){
-    const [show, setShow] = useState(false)
-    const [data, setData] = useState([])
+  const [show, setShow] = useState(false)
+  const [data, setData] = useState([])
 
-    const handleShow = () => setShow(true)
-    const handleClose = () => setShow(false)
-    useEffect(() => {
-      const fetchData = async () => {
-          try {
-            const result = await axios(`${url}/view/Nissan`)
-            setData(result.data)
-          } catch (error) {
-            console.log(error)
-          }
-      }
-      fetchData()
-      //const interval = setInterval(fetchData, 1000)
-      //return clearInterval(interval)
-    }, [url])
-    function handleSubmit(e) {
-        e.preventDefault()
-        axios.post(`${url}/create/${carname.value}`, {
-          car_name: carname.value,
-          car_mileage: mileage.value,
-          txn_date: new Date().toString(),
-          txn_total: total.value,
-          txn_gas_total: gastotal.value 
-        }).then(function (response) {
-          console.log(response)
-/*          setData(data.concat({
-            id: response.data.id,
-            car_name: carname.value,
-            car_mileage: mileage.value,
-            txn_date: Date.now(),
-            txn_total: total.value,
-            txn_gas_total: gastotal.value 
-          })) */
-        }).catch(function (error) {
-          console.warn(error)
-        })
-        setShow(false)
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false)
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+          const result = await axios(`${url}/view/Nissan`)
+          setData(result.data)
+        } catch (error) {
+          console.log(error)
+        }
     }
-    return {
-        data,
-        form: {
-          carname,
-          mileage,
-          total,
-          gastotal,
-          handleSubmit,
-        },
-        modal:{
-          show,
-          handleShow, 
-          handleClose
-        },
+    fetchData()
+    //const interval = setInterval(fetchData, 1000)
+    //return clearInterval(interval)
+  }, [url])
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios.post(`${url}/create/${carname.value}`, {
+      car_name: carname.value,
+      car_mileage: mileage.value,
+      txn_date: new Date().toString(),
+      txn_total: total.value,
+      txn_gas_total: gastotal.value 
+    }).then(function (response) {
+      console.log(response)
+/*      setData(data.concat({
+        id: response.data.id,
+        car_name: carname.value,
+        car_mileage: mileage.value,
+        txn_date: Date.now(),
+        txn_total: total.value,
+        txn_gas_total: gastotal.value 
+      })) */
+    }).catch(function (error) {
+      console.warn(error)
+    })
+    setShow(false)
+  }
+  return {
+    data,
+    form: {
+      carname,
+      mileage,
+      total,
+      gastotal,
+      handleSubmit,
+    },
+    modal:{
+      show,
+      handleShow, 
+      handleClose
     }
+  }
 }
 
 export default useController
